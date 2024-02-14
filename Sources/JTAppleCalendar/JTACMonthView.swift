5364c8cd4df4a27bbe757022d3259278542e383c
Robert Crabtree <robert.crabtree@gmail.com>
2024-02-14 10:22:34 -0800

    Fix month view force unwrap crash

--- Sources/JTAppleCalendar/JTACMonthView.swift
+++ Sources/JTAppleCalendar/JTACMonthView.swift
@@ -130,8 +130,13 @@ open class JTACMonthView: UICollectionView {
         // Ensure date is within valid boundary
         let components = calendar.dateComponents([.year, .month, .day], from: date)
         let firstDayOfDate = calendar.date(from: components)!
-        if !((firstDayOfDate >= startOfMonthCache!) && (firstDayOfDate <= endOfMonthCache!)) { return retval }
-        
+
+        guard let startOfMonthCache,
+              let endOfMonthCache,
+              firstDayOfDate >= startOfMonthCache && firstDayOfDate <= endOfMonthCache else {
+            return retval
+        }
+
         // Get valid indexPath of date to scroll to
         let retrievedPathsFromDates = pathsFromDates([date])
         if retrievedPathsFromDates.isEmpty { return retval }
